f537d97744a0605fbc5197b3093ad76466c669a4
Amar <108946547+AmarVcode@users.noreply.github.com>
2024-06-14 03:17:56 +0530

    1 hero done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-function mytabside(i){
 
-}
+var animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-container'),
+    renderer: 'canvas',
+    loop: true,
+    autoplay: true,
+    path: 'json/go-btn.json',
+    rendererSettings: {
+        preserveAspectRatio: 'xMidYMid meet', // Adjust as needed to fit within the container
+        progressiveLoad: true,
+        hideOnTransparent: true
+    }
+});
